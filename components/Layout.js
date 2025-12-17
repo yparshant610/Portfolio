416912905984b4b6c65d2e75cbf8dab545e5be58
yparshant610 <yparshant610@gmail.com>
2025-12-17 10:54:37 +0530

    corrected the dullness of whole view- the error was i placed opacity 50 on layout that was to be placed on the image that is placed to top_left side

--- components/Layout.js
+++ components/Layout.js
@@ -15,7 +15,7 @@ import TopLeftImg from '../components/TopLeftImg'
 
 
 const Layout = ({children}) => {
-  return ( <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative opacity-50`}
+  return ( <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
   >
     <TopLeftImg />
     <Nav />
