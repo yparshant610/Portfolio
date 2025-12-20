4c97d94b97f496ea49f64be2089f8e6af5c87815
yparshant610 <yparshant610@gmail.com>
2025-12-20 11:02:35 +0530

    Making About page

--- components/Socials.js
+++ components/Socials.js
@@ -15,25 +15,25 @@ import {
 const Socials = () => {
   return (
     <div className='flex items-center gap-x-5 text-lg'>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://github.com/yparshant610'} className='hover:text-accent transition-all duration-300'>
         <RiGithubLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.linkedin.com/in/parshant-yadav-463aa6201'} className='hover:text-accent transition-all duration-300'>
         <RiLinkedinLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.instagram.com/pras_hant610/'} className='hover:text-accent transition-all duration-300'>
         <RiInstagramLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.youtube.com/@ALphaGaming-fo1dt'} className='hover:text-accent transition-all duration-300'>
         <RiYoutubeLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.behance.net/parshantyadav3'} className='hover:text-accent transition-all duration-300'>
         <RiBehanceLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://www.facebook.com/share/1G3LqABCX9/'} className='hover:text-accent transition-all duration-300'>
         <RiFacebookLine />
       </Link>
-      <Link href={''} className='hover:text-accent transition-all duration-300'>
+      <Link href={'https://x.com/Parshant4169569?s=09'} className='hover:text-accent transition-all duration-300'>
         <RiTwitterLine />
       </Link>
     </div>
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,4 @@
-
+import React, { useState } from "react";
 
 // icons
 import {
@@ -15,16 +15,16 @@ import {
   SiFramer,
   SiAdobexd,
   SiAdobephotoshop,
+  SiAdobeillustrator
 } from "react-icons/si";
-
 
 //  data
 export const aboutData = [
   {
-    title: 'skills',
+    title: "skills",
     info: [
       {
-        title: 'Web Development',
+        title: "Web Development",
         icons: [
           <FaHtml5 />,
           <FaCss3 />,
@@ -36,67 +36,137 @@ export const aboutData = [
         ],
       },
       {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        title: "UI/UX Design",
+        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />, <SiAdobeillustrator/>],
       },
     ],
   },
   {
-    title: 'awards',
+    title: "awards",
     info: [
       {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
+        title: "Webby Awards - Honoree",
+        stage: "2011 - 2012",
       },
       {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
+        title: "Adobe Design Achievement Awards - Finalist",
+        stage: "2009 - 2010",
       },
     ],
   },
   {
-    title: 'experience',
+    title: "experience",
     info: [
       {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
+        title: "UX/UI Designer - XYZ Company",
+        stage: "2012 - 2023",
       },
       {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
+        title: "Web Developer - ABC Agency",
+        stage: "2010 - 2012",
       },
       {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
+        title: "Intern - DEF Corporation",
+        stage: "2008 - 2010",
       },
     ],
   },
   {
-    title: 'credentials',
+    title: "credentials",
     info: [
       {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
+        title: "Web Development - ABC University, LA, CA",
+        stage: "2011",
       },
       {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
+        title: "Computer Science Diploma - AV Technical Institute",
+        stage: "2009",
       },
       {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
+        title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
+        stage: "2006",
       },
     ],
   },
 ];
 
+// Components
+import Avatar from "../../components/Avatar";
+import Circles from "../../components/Circles";
 
-
-
+//framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
 
 const About = () => {
+  const [index, setIndex] = useState(0);
+  console.log(index);
 
-  return <div>About</div>;
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles />
+      {/* avatar img*/}
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px] "
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        {/* text */}
+        <div className="flex-1 flex flex-col justify-center">
+          <h2>Captivating <span>stories</span> birth magnificent designs.</h2>
+          <p>Hi, I am parshant yadav, a web developer and UI/UX designer with a passion for creating beautiful, functional, and user centered digital experiences. I am always looking for new and innovative ways to bring my clients visions to life.</p>
+          
+          <br/>
+          
+          <p>I believe that design is about more than just making things look pretty, it is about solving problems and creating intuitive, enjoyable experiences for users.</p>
+          <br/>
+          <p>Whether I am working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
+        </div>
+        {/* info */}
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex &&
+                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start ">
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div key={itemIndex} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'>
+                  {/* title */}
+                  <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+                  <div className="hidden md:flex">-</div>
+                  <div>{item.stage}</div>
+                  <div className='flex gap-x-4'>
+                    {/* icons */}
+                    {item.icons?.map((icon, itemIndex) =>{
+                      return <div className=' text-2xl text-white'>{icon}</div>
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
