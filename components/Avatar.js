f9a903aaf5645cbe6685c305158a58395335e29c
yparshant610 <yparshant610@gmail.com>
2025-12-21 13:31:22 +0530

    changed image name for resolving the issue in vercel

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none ">
       <Image
-        src={"/Avatar.png"}
+        src={"/avatar.png"}
         width={738}
         height={672}
         alt=""
